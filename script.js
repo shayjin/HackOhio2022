44213b289bad2fa65b99d9d425f73414ff4f8d47
Shin <shin.810@osu.edu>
2022-10-08 22:03:11 -0400

    calculation error detected/fixed (wuke)

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const morning = [6, 12];
 const afternoon = [12, 18];
 const night = [18, 0];
 
+function isFloat(n){
+    return Number(n) === n && n % 1 !== 0;
+}
+
 $('document').ready(function(){
     $('#d').on('change', function() {
         $('button[id=submit]').attr('value', $(this).find(":selected").attr('value'));
@@ -82,13 +86,21 @@ function analysis(button) {
 
     */
 
-    var cols = [0]
+    var cols = [0];
+    var colsName = [];
 
     for (var i = 1; i < dataset[0].length; i++) {
         if (dataset[0][i].includes(dormName)) {
             cols.push(i);
+            colsName.push(dataset[0][i]);
         }
     }
+
+    var allTotalSums = [];
+    var allMidNightSums = [];
+    var allMorningSums = [];
+    var allAfternoonSums = [];
+    var allNightSums = [];
 
     var totalSums = [];
     var midNightSums = [];
@@ -108,62 +120,147 @@ function analysis(button) {
     var afternoonDays = 0.0;
     var nightDays = 0.0;
 
-    for (var i = 1; i < cols.length; i++) {
-        var totalSum = 0;
-        var midNightSum = 0;
-        var morningSum = 0;
-        var afternoonSum = 0;
-        var nightSum = 0;
+    var allDays = 0.0;
+    var allMidNightDays = 0.0;
+    var allMorningDays = 0.0;
+    var allAfternoonDays = 0.0;
+    var allNightDays = 0.0;
+    
+    var colIndex = 1;
+    for (var i = 1; i < dataset[0].length; i++) {
+        var totalSum = 0.0;
+        var midNightSum = 0.0;
+        var morningSum = 0.0;
+        var afternoonSum = 0.0;
+        var nightSum = 0.0;
+
+        var allTotalSum = 0.0;
+        var allMidNightSum = 0.0;
+        var allMorningSum = 0.0;
+        var allAfternoonSum = 0.0;
+        var allNightSum = 0.0;
 
         for (var j = 1; j < dataset.length; j++) {
-            var value = parseInt(dataset[j][cols[i]]);
+            var value = parseFloat(dataset[j][i]);
+            if (cols.includes(i)) {
+                if (Number.isInteger(value) || isFloat(value)) {
+                    totalSum += value;
+                    days += 1.0;
+                    
+                    var regex = /([0-1][0-9]|2[0-3]):00:00/g;
 
+                    var time = String(dataset[j][0].match(regex));
+                    time = time.substring(0, time.indexOf(':'));
 
-            if (Number.isInteger(value)) {
-                totalSum += value;
-                days += 1;
-                
+                    if (time >= midnight[0] && time < midnight[1]) {
+                        midNightSum += value;
+                        midNightDays += 1;
+                    } else if (time >= morning[0] && time < morning[1]) {
+                        morningSum += value;
+                        morningDays += 1;
+                    } else if (time >= afternoon[0] && time < afternoon[1]) {
+                        afternoonSum += value;
+                        afternoonDays += 1;
+                    } else if (time >= night[0]) {
+                        nightSum += value;
+                        nightDays += 1;
+                    }
+                }
+            }
+
+            
+            if (Number.isInteger(value) || isFloat(value)) {
+                allTotalSum += value;
+                allDays += 1.0;
+                    
                 var regex = /([0-1][0-9]|2[0-3]):00:00/g;
-
                 var time = String(dataset[j][0].match(regex));
                 time = time.substring(0, time.indexOf(':'));
 
                 if (time >= midnight[0] && time < midnight[1]) {
-                    midNightSum += value;
-                    midNightDays += 1;
+                    allMidNightSum += value;
+                    allMidNightDays += 1;
                 } else if (time >= morning[0] && time < morning[1]) {
-                    morningSum += value;
-                    morningDays += 1;
+                    allMorningSum += value;
+                    allMorningDays += 1;
                 } else if (time >= afternoon[0] && time < afternoon[1]) {
-                    afternoonSum += value;
-                    afternoonDays += 1;
+                    allAfternoonSum += value;
+                    allAfternoonDays += 1;
                 } else if (time >= night[0]) {
-                    nightSum += value;
-                    nightDays += 1;
+                    allNightSum += value;
+                    allNightDays += 1;
                 }
             }
+
+        }
+
+        if (cols.includes(i)) {
+            totalSums.push(totalSum / days);
+            midNightSums.push(midNightSum / midNightDays);
+            morningSums.push(morningSum / morningDays);
+            afternoonSums.push(afternoonSum / afternoonDays);
+            nightSums.push(nightSum / nightDays);
         }
 
 
-       // console.log(totalSum);
-        totalSums.push(totalSum / days);
-        midNightSums.push(midNightSum / midNightDays);
-        morningSums.push(morningSum / morningDays);
-        afternoonSums.push(afternoonSum / afternoonDays);
-        nightSums.push(nightSum / nightDays);
-
-      //  console.log(totalSum);
-     //   console.log(midNightSum);
-       // console.log(morningSum);
-       // console.log(afternoonSum);
-       // console.log(nightSum);
+        allTotalSums.push(allTotalSum / allDays);
+        allMidNightSums.push(allMidNightSum / allMidNightDays);
+        allMorningSums.push(allMorningSum / allMorningDays);
+        allAfternoonSums.push(allAfternoonSum / allAfternoonDays);
+        allNightSums.push(allNightSum / allNightDays);
 
         days = 0.0;
         midNightDays = 0.0;
         morningDays = 0.0
         afternoonDays = 0.0;
         nightDays = 0.0;
+
+        allDays = 0.0;
+        allMidNightDays = 0.0;
+        allMorningDays = 0.0;
+        allAfternoonDays = 0.0;
+        allNightDays = 0.0;
+        
+        console.log(totalSums);
+        console.log(allTotalSums);
     }
+
+/*
+    console.log(allTotalSums);
+    console.log(allMidNightSums);
+    console.log(allMorningSums);
+    console.log(allAfternoonSums);
+    console.log(allNightSums);
+*/
+    
+    var modifiedAllTotalSums = [];
+
+    for (var i = 1; i < cols.length; i++) {
+        var count = 0.0;
+        var sum2 = 0.0;
+
+        var category = dataset[0][cols[i]];
+        category = category.substring(category.indexOf(' - ') + 3);
+        console.log(category);
+
+        for (var j = 1; j < allTotalSums.length; j++) {
+            if (dataset[0][j].includes(category)) {
+                var num = parseFloat(allTotalSums[j-1]);
+                console.log(dataset[0][j] + " "+ num + " " + j);
+                if (num || num == 0) {
+                    sum2 += num;
+                    count += 1.0;
+                    console.log('nice');
+                }
+                console.log(sum2  + " " +  count);
+            }
+        }
+        modifiedAllTotalSums.push(sum2 / count);
+    }
+    
+    console.log(modifiedAllTotalSums);
+    console.log(totalSums);
+    
 
     for (var i = 1; i < weatherDataset[0].length; i++) {
         var totalWeather = 0.0;
@@ -180,7 +277,7 @@ function analysis(button) {
         nightDays = 0.0;
 
         for (var j = 1; j < weatherDataset.length; j++) {
-            var value = parseInt(weatherDataset[j][i]);
+            var value = parseFloat(weatherDataset[j][i]);
 
             if (Number.isInteger(value)) {
                 totalWeather += value;
@@ -220,7 +317,6 @@ function analysis(button) {
     console.log(afternoonWeathers);
     console.log(nightWeathers);
 
-    const energies = document.getElementById('energy');
 
     for (var i = 1; i < cols.length; i++) {
         var amount = totalSums[i-1];
